354c7d513b392e1dfb8a0bbc24be2d1ff443ab78
amboz <abozigian@gmail.com>
2017-10-09 15:34:39 -0700

    Updated formatting per style guide

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,25 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if(Array.isArray(obj)){
+  if (Array.isArray(obj)) {
     var output = [];
-    for(var i = 0; i < obj.length; i++){
+    for (var i = 0; i < obj.length; i++) {
       output.push(stringifyJSON(obj[i]));
     }
     return '[' + output.join(',') + ']';
   }
-  if(typeof obj === 'object' && obj){
+  if (typeof obj === 'object' && obj) {
     var output = [];
-    for(var key in obj){
-      if(typeof obj[key] === 'function' || obj[key] === undefined){
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
         continue;
       } else {
-            output.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        output.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
       }
     }
     return '{' + output.join(',') + '}';
   }
-  if(typeof obj === 'string'){
+  if (typeof obj === 'string') {
     return '"' + obj + '"';
   }
   return '' + obj;
